69457b540133fe8f82e745fa86928bae3e7792c8
Jianhong-Xu69 <114007102+Jianhong-Xu69@users.noreply.github.com>
2022-09-20 11:32:02 -0400

    Create Sample.java

--- src/Sample.java
+++ src/Sample.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class Sample {
     public static void main(String[] args) {
         Scanner heh = new Scanner(System.in);
+        System.out.println("Your name is?");
         String name = heh.nextLine();
-        System.out.println("Jianhong Xu");
+        System.out.println("Hello " + name);
+        System.out.println("Scanner my beloved");
     }
 }
