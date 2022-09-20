f3de578a653448f64f7479d125ee7c485022499c
unknown <BT_4N2_18@BKTECH.LOCAL>
2022-09-20 11:23:50 -0400

    Initial commit

---
+++ src/Sample.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Sample {
+    public static void main(String[] args) {
+        Scanner heh = new Scanner(System.in);
+        String name = heh.nextLine();
+        System.out.println("Jianhong Xu");
+    }
+}
